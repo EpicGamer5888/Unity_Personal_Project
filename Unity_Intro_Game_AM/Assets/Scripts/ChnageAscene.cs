3c8f29f2b98214ce6585e5fd8bfb1f0068ce2c37
ThtWasBrutal <eunruh73@gmail.com>
2024-10-23 10:24:32 -0700

    Demo Game

--- Unity_Intro_Game_AM/Assets/Scripts/ChnageAscene.cs
+++ Unity_Intro_Game_AM/Assets/Scripts/ChnageAscene.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class ChangeAscene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(3);
         }
-       
+
     }
 
 }
-

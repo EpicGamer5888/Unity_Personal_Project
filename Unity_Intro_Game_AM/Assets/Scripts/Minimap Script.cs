06c53bb89dc74d0df68ddcb997f2af22caca5ec8
EpicGamer5888 <63515818+EpicGamer5888@users.noreply.github.com>
2025-01-30 10:23:34 -0800

    Minimap

---
+++ Unity_Intro_Game_AM/Assets/Scripts/Minimap Script.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    public Transform Player;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = Player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;   
+        
+
+        transform.rotation = Quaternion.Euler(-90f, Player.eulerAngles.y, 0f);
+    }
+ 
+}
+
